a23ca7b7a75c97e0a852a3e6dc044a8440769da0
flea-market <samat.dogdurbaev@gmail.com>
2015-03-29 04:25:38 +0600

    Sunday  commit

--- application/apps/models/mailbox_model.php
+++ application/apps/models/mailbox_model.php
@@ -1,3 +1,55 @@
 <?php
+namespace apps\models;
+use lib\completed_classes\registry;
+class mailbox_model{
 
+    function __construct(){
+	
+    }
+    
+    function deleteMail(){
+	
+    }
+    
+    function clearMailBox(){
+	
+    }
+    
+    function getMail(){
+	$this->connect->beginTransaction();
+	try{
+	    $stmnt = $this->connect->prepare('SELECT * FROM `user_mail` WHERE ');
+	    /*
+	    $variable->bindParam();
+	    parametres of sql-query
+	    */
+	    $stmnt->execute();
+	    $this->connect->commit();
+	    $result = $stmnt->fetchAll(PDO::FETCH_ASSOC);
+	    return $result;
+	} 
+	catch (PDOException $e ){
+	    $this->connect->rollBack();
+	    return 'ERROR: '.$e->getMessage();
+	}
+    }
+    
+    function sendMail( $array ){
+	$this->connect->beginTransaction();
+	try{
+	    $stmnt = $this->connect->prepare('INSERT INTO ``.`` VALUES()');  //insert sql query
+	    /*
+	    $variable->bindParam();
+	    parametres of sql-query
+	    */
+	    $stmnt->execute();
+	    $this->connect->commit();
+	} 
+	catch (PDOException $e ){
+	    $this->connect->rollBack();
+	    return 'ERROR: '.$e->getMessage();
+	}
+    }
+    
+}
 ?>
